0a83e16852c7d1b968b146d0eddb8bd4a35d3902
Deforest <270113476@qq.com>
2020-03-16 10:40:56 +0800

    AC

--- 面试题 01.06/compressString.go
+++ 面试题 01.06/compressString.go
@@ -1,1 +1,27 @@
 package 面试题_01_06
+
+import "strconv"
+
+func compressString(S string) string {
+	if len(S) == 0 {
+		return S
+	}
+	countWords := 0
+	nowWords := int32(S[0])
+	var resWords string
+	for _, i := range S {
+		if nowWords != i {
+			resWords += string(nowWords) + strconv.Itoa(countWords)
+			nowWords = i
+			countWords = 0
+		}
+		countWords++
+	}
+	resWords += string(nowWords) + strconv.Itoa(countWords)
+
+	if len(resWords) >= len(S) {
+		return S
+	} else {
+		return resWords
+	}
+}
